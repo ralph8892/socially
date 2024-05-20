632b50f3f5505e9c194e52d029eb635556bd53a1
ralph8892 <brufalralph@gmail.com>
2024-05-20 21:53:29 +0800

    Enhanced security for database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,10 +10,12 @@ class UserController extends Controller
 {
     public function register (Request $request) {
         $incomingFields = $request->validate([
-            'username' => ['required', 'min:5', 'max:20', Rule::unique('users', 'username')],
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
             'password' => ['required', 'min:9', 'confirmed']
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
 
         User::create($incomingFields);
         return 'Hello from register function';
