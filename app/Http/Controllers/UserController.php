2599e28948f6f40fb288be14a56d2b7cdb6fcf2a
ralph8892 <brufalralph@gmail.com>
2024-05-21 23:43:35 +0800

    Full back-end functionality for login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,14 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function showCorrectHomepage () {
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {    
+            return view('homepage');
+        }
+    }
+
     public function login (Request $request) {
         $incomingFields = $request->validate([
             'loginusername' => 'required',
@@ -15,6 +23,7 @@ class UserController extends Controller
         ]);
 
         if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
             return 'Congrats';
         } else {
             return 'Error logging in';
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExampleController;
 
-Route::get('/', [ExampleController::class, "homepage"]);
+Route::get('/', [ExampleController::class, "showCorrectHomepage"]);
 Route::get('/about', [ExampleController::class, "aboutPage"]);
 
 Route::post('/register', [UserController::class, "register"]);
