409644225c21a7ab150cd20905a038c8318bb695
ralph8892 <brufalralph@gmail.com>
2024-05-24 03:50:59 +0800

    Fully functional blog post module complete with front-end and back-end

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function storeNewPost () {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+
+        Post::create($incomingFields);
+
         return "Hey there!";
     }
 
--- database/migrations/2024_05_23_155515_create_posts_table.php
+++ database/migrations/2024_05_23_155515_create_posts_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->longText('body');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
         });
     }
 
