7f6d7565aa2ae6b66eeadc3c9bc2f73988d9e4e7
ralph8892 <brufalralph@gmail.com>
2024-05-23 23:41:38 +0800

    Initial setup for blog posts module

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
+// User related routes
 Route::get('/', [ExampleController::class, "showCorrectHomepage"]);
-
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, "logout"]);
+
+// Blog posts related routes
+Route::get('/create-post', [PostController::class, "showCreateForm"]);
