5d5bad5a5f1bccd9c6ec43db2fef80184e8fe7fc
ralph8892 <brufalralph@gmail.com>
2024-05-30 06:45:41 +0800

    Improved functionality for updating and deleting blogposts

--- resources/views/single-post.blade.php
+++ resources/views/single-post.blade.php
@@ -4,7 +4,7 @@
 			<h2>{{$post->title}}</h2>
 			@can('update', $post)
 			<span class="pt-2">
-			  <a href="#" class="text-primary mr-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-edit"></i></a>
+			  <a href="/post/{{ $post->id }}/edit" class="text-primary mr-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-edit"></i></a>
 			  <form class="delete-post-form d-inline" action="/post/{{ $post->id }}" method="POST">
 				@csrf
 				@method('DELETE')
@@ -12,14 +12,14 @@
 			  </form>
 			</span>
 			@endcan
-		  </div>
+		</div>
 
-		  <p class="text-muted small mb-4">
+		<p class="text-muted small mb-4">
 			<a href="#"><img class="avatar-tiny" src="https://gravatar.com/avatar/f64fc44c03a8a7eb1d52502950879659?s=128" /></a>
 			Posted by <a href="#">{{$post->user->username}}</a> on {{$post->created_at->format('n/j/Y')}}
-		  </p>
+		</p>
 
-		  <div class="body-content">
+		<div class="body-content">
 			{{$post->body}}
 		</div>
     </div>
