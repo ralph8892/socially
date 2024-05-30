e5d3769cc460f886ce495c93c0d86a734b40cff7
ralph8892 <brufalralph@gmail.com>
2024-05-31 01:21:36 +0800

    Persist uploaded data into database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,6 +20,17 @@ class UserController extends Controller
         $imgData = Image::make($request->file('avatar'))->fit(120)->encode('jpg');
 
         Storage::put('public/avatars' . $filename, $imgData);
+
+        $oldAvatar = $user->avatar;
+
+        $user->avatar = $filename;
+        $user->save();
+
+        if ($oldAvatar != "/fallback-avatar.jpg") {
+            Storage::delete(str_replace("/storage/", "public/", $oldAvatar));
+        }
+
+        return back()->with("success", "Avatar successfully updated!");
     }
 
     public function showAvatarForm () {
@@ -27,7 +38,7 @@ class UserController extends Controller
     }
 
     public function profile (User $user) {
-        return view('profile-posts', ['username' => $user->username, 'posts' => $user->posts()->latest()->get(), 'postCount' => $user->posts()->count()]);
+        return view('profile-posts', ['avatar' => $user->avatar, 'username' => $user->username, 'posts' => $user->posts()->latest()->get(), 'postCount' => $user->posts()->count()]);
     }
 
     public function logout () {
