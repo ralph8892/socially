cb7805e62af3c72ac1694e766f5dfa2837ecb5b3
ralph8892 <brufalralph@gmail.com>
2024-06-04 03:00:23 +0800

    Dynamic document page titles and post components

--- resources/views/homepage-feed.blade.php
+++ resources/views/homepage-feed.blade.php
@@ -4,10 +4,7 @@
         <h2 class="text-center mb-4">Latest Posts</h2>
         <div class="list-group">
             @foreach($posts as $post)
-            <a href="/post/{{ $post->id }}" class="list-group-item list-group-item-action">
-                <img class="avatar-tiny" src="{{ $post->user->avatar }}" />
-                <strong>{{ $post->title }}</strong> <span class="text-muted small">by {{ $post->user->username }} on {{ $post->created_at->format('n/j/Y') }}</span>
-            </a>
+            <x-post :post="$post" />
             @endforeach
         </div>
         <div class="mt-4">
@@ -15,7 +12,7 @@
         </div>
         @else
         <div class="text-center">
-            <h2>Hello <strong>{{auth()->user()->username}}</strong>, your feed is empty.</h2>
+            <h2>Hello <strong>{{ auth()->user()->username }}</strong>, your feed is empty.</h2>
             <p class="lead text-muted">Your feed displays the latest posts from the people you follow. If you don&rsquo;t have any friends to follow that&rsquo;s okay; you can use the &ldquo;Search&rdquo; feature in the top menu bar to find content written by people with similar interests and then follow them.</p>
         </div>
         @endunless     
