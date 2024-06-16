ee48518442d4d647eea1da96e1c28df3c73fe4bb
ralph8892 <brufalralph@gmail.com>
2024-06-16 19:31:52 +0800

    Implemented API endpoints

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -44,6 +44,11 @@ class PostController extends Controller
         return redirect('/profile/'. auth()->user()->username)->with('success', 'Post successfully deleted');
     }
 
+    public function deleteApi(Post $post) {
+        $post->delete();
+        return 'true';
+    }
+
     public function viewSinglePost (Post $post) {
         return view('single-post', ['post' => $post]);
     }
@@ -63,6 +68,23 @@ class PostController extends Controller
         dispatch(new SendNewPostEmail(['sendTo' => auth()->user()->email, 'name' => auth()->user()->username, 'title' => $newPost->title]));
 
         return redirect("/post/{$newPost->id}")->with('success', 'New Blogpost Created');
+    }
+
+    public function storeNewPostApi(Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+
+        $newPost = Post::create($incomingFields);
+
+        dispatch(new SendNewPostEmail(['sendTo' => auth()->user()->email, 'name' => auth()->user()->username, 'title' => $newPost->title]));
+
+        return $newPost->id;
     }
 
     public function showCreateForm () {
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -106,6 +106,21 @@ class UserController extends Controller
         }
     }
 
+    public function loginApi (Request $request) {
+        $incomingFields = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($incomingFields)) {
+            $user = User::where('username', $incomingFields['username'])->first();
+            $token = $user->createToken('ourapptoken')->plainTextToken;
+            return $token;
+        }
+
+        return 'Invalid action';
+    }
+
     public function login (Request $request) {
         $incomingFields = $request->validate([
             'loginusername' => 'required',
